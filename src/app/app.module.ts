a2580bcbe39ae8c95585b5eb98a097fc40b52835
katy2020 <katayoun@wavelet.net>
2020-07-26 05:36:02 +0800

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+//import { MatToolbarModule } from '@angular/material/toolbar'; 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule, MatCardModule, MatGridListModule } from 
+'@angular/material';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+     MatGridListModule,
+     MatListModule,
+     MaterialModule
     //FlexLayoutModule
     
   ],
